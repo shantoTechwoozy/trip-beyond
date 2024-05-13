ab1476c8d4a5685775f4064a62eb5dc0dbbcb171
shantoTechwoozy <shanto.techwoozy@gmail.com>
2024-05-13 13:39:24 +0600

    13th commit

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,15 +8,11 @@ export default function Home() {
       <p className="text-center text-justify max-w-lg md:max-w-none decoration-solid">
         {" "}
         {/* Limit text width on larger screens */}
-        TechoWoozy is a prominent software development and digital solutions
-        agency committed to empowering businesses to excel in the dynamic
-        digital realm. With a seasoned team of experts and a fervor for
-        pioneering solutions, we excel in delivering customized software support
-        to guarantee flawless operations. Additionally, our proficiency extends
-        to crafting cutting-edge digital marketing strategies tailored to meet
-        the distinct requirements of our clientele. At EchoWoozy, we are
-        dedicated to driving success and fostering growth through innovative
-        technologies and strategic digital initiatives.
+        Unlock the extraordinary with Trip Beyond, your passport to unparalleled
+        adventures. From secluded beaches to majestic peaks, our expert team
+        crafts tailor-made journeys that redefine travel. Immerse yourself in
+        culture, savor unique experiences, and create memories that last a
+        lifetime. Join us and journey beyond the ordinary with Trip Beyond.
       </p>
       <Button className="mt-4" variant="custom">
         Read more
