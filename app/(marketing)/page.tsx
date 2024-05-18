9ae441f03266044f81ca52984d8497fdd41a08f9
shantoTechwoozy <shanto.techwoozy@gmail.com>
2024-05-18 13:25:25 +0600

    loading added in read more button

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useState } from "react";
 import { DealsCard } from "@/components/card-sections/deals";
 import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleReadMoreClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
     <main className="flex flex-col items-center justify-center px-4 md:p-8 lg:p-10">
       <p className="text-justify max-w-lg md:max-w-none text-base md:text-lg lg:text-xl decoration-solid mt-10">
@@ -11,12 +23,22 @@ export default function Home() {
         culture, savor unique experiences, and create memories that last a
         lifetime. Join us and journey beyond the ordinary with Trip Beyond.
       </p>
-      <Button
-        className="rounded-full bg-[#FF9C00] hover:bg-orange-500/90 active:border-b-0 text-black px-4 py-2 sm:py-3 md:py-4 mt-5"
-        variant="custom"
-      >
-        Read more
-      </Button>
+      {isLoading ? (
+        <Button
+          disabled
+          className="rounded-full bg-[#FF9C00] hover:bg-orange-500/90 active:border-b-0 text-black px-4 py-2 sm:py-3 md:py-4 mt-5"
+        >
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </Button>
+      ) : (
+        <Button
+          className="rounded-full bg-[#FF9C00] hover:bg-orange-500/90 active:border-b-0 text-black px-4 py-2 sm:py-3 md:py-4 mt-5"
+          onClick={handleReadMoreClick}
+        >
+          Read more
+        </Button>
+      )}
       <div className="w-full max-w-screen-lg mx-auto mt-5 mb-5">
         <h1 className="text-xl md:text-2xl lg:text-xl text-center font-bold mb-5 mt-8">
           Find Great Deals, Only for you
